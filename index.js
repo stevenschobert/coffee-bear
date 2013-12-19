511581802cbe0c37d997c572ffe83dee0acff30f
Steven Schobert <spschobert@gmail.com>
2013-12-19 14:12:35 -0600

    get measurements by cup

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ var express = require('express'),
 // configuration
 app.set('port', process.env.PORT || 5000);
 
-app.get('/api/measurements', function (req, res) {
+app.get('/api/measurements/:cups?', function (req, res) {
   coffeeBear(function (err, data) {
-    if (err) {
-      res.json(500, {error: err});
+    var cups = req.params.cups;
+
+    if (err) { res.json(500, {error: err}); }
+
+    console.log(cups);
+
+    if (cups === undefined) {
+      res.json(data);
+      return;
     }
-    res.json(data);
+
+    if (data[cups] !== undefined) {
+      res.json(data[cups]);
+      return;
+    }
+
+    res.json(404, {error: 'Measurement not found' });
   });
 });
 
