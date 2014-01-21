af311f434097a645fad47bf67df20726ecd19748
Steven Schobert <spschobert@gmail.com>
2014-01-21 10:22:24 -0600

    strict javascripts

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-// dependencies
-var request = require('request'),
-    STATUS_CODES = require('http').STATUS_CODES,
-    cheerio = require('cheerio'),
+(function() {
+  'use strict';
 
-    tableToMeasurements = function tableToMeasurements (table) {
-      var cupRows = table.find('tr').slice(3,-1),
-          coffeeNames = ['ounces', 'grams', 'teaspoons', 'tablespoons', 'cups'],
-          waterNames = ['fluidounces', 'cups', 'pints', 'quarts', 'halfgallons', 'milliliters', 'liters'],
-          cups = {};
+  // dependencies
+  var request = require('request'),
+      STATUS_CODES = require('http').STATUS_CODES,
+      cheerio = require('cheerio'),
 
-      cupRows.each(function (i, elem) {
-        var cells = cheerio(elem).find('td'),
-            coffee = cells.slice(1, 6),
-            water = cells.slice(6, cells.length);
+      tableToMeasurements = function tableToMeasurements (table) {
+        var cupRows = table.find('tr').slice(3,-1),
+            coffeeNames = ['ounces', 'grams', 'teaspoons', 'tablespoons', 'cups'],
+            waterNames = ['fluidounces', 'cups', 'pints', 'quarts', 'halfgallons', 'milliliters', 'liters'],
+            cups = {};
 
-        cups[i+1] = { 'coffee': {}, 'water': {} };
+        cupRows.each(function (i, elem) {
+          var cells = cheerio(elem).find('td'),
+              coffee = cells.slice(1, 6),
+              water = cells.slice(6, cells.length);
 
-        coffee.each(function (j, elem) {
-          cups[i+1].coffee[coffeeNames[j]] = cheerio(elem).text();
+          cups[i+1] = { 'coffee': {}, 'water': {} };
+
+          coffee.each(function (j, elem) {
+            cups[i+1].coffee[coffeeNames[j]] = cheerio(elem).text();
+          });
+          water.each(function (j, elem) {
+            cups[i+1].water[waterNames[j]] = cheerio(elem).text();
+          });
         });
-        water.each(function (j, elem) {
-          cups[i+1].water[waterNames[j]] = cheerio(elem).text();
-        });
-      });
 
-      return cups;
-    },
+        return cups;
+      },
 
-    brew = function brew (cb) {
-      var pour = function pour (err, res, body) {
-        var $, standard;
+      brew = function brew (cb) {
+        var pour = function pour (err, res, body) {
+          var $, standard;
 
-        if (err) {
-          return cb(err);
-        }
+          if (err) {
+            return cb(err);
+          }
 
-        if (res.statusCode !== 200) {
-          return cb(new Error(STATUS_CODES[res.statusCode]));
-        }
+          if (res.statusCode !== 200) {
+            return cb(new Error(STATUS_CODES[res.statusCode]));
+          }
 
-        $ = cheerio.load(body);
-        standard = $('#standard').parent().next();
-        //connoisseur = $('#connoisseur').parent().next();
+          $ = cheerio.load(body);
+          standard = $('#standard').parent().next();
+          //connoisseur = $('#connoisseur').parent().next();
 
-        cb(null, tableToMeasurements(standard));
+          cb(null, tableToMeasurements(standard));
+        };
+
+        request('http://blackbearcoffee.com/resources/83', pour);
       };
 
-      request('http://blackbearcoffee.com/resources/83', pour);
-    };
-
-module.exports = brew;
+  module.exports = brew;
+})();
